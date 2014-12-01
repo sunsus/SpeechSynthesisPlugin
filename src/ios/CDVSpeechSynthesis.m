e31fc7e9c8157dc55cc44ea5df971b6695d18b79
sunsus <patrick@sunsus.net>
2014-12-01 09:22:26 +0100

    Update CDVSpeechSynthesis.m

--- src/ios/CDVSpeechSynthesis.m
+++ src/ios/CDVSpeechSynthesis.m
@@ -1,19 +1,142 @@
 // http://docs.phonegap.com/en/3.5.0/guide_platforms_ios_plugin.md.html#iOS%20Plugins
+#import "CDVSpeechSynthesis.h"
 
-@implementation CDVLocationManager {
 
+@implementation CDVSpeechSynthesis
+
+- (void) continueSpeakingSyntheticSpeaking{
+    [self.speechSynthesizer continueSpeaking];
 }
 
 
-  - (void)myMethod:(CDVInvokedUrlCommand*)command
-  {
-      CDVPluginResult* pluginResult = nil;
-      NSString* myarg = [command.arguments objectAtIndex:0];
+- (void) stopSyntheticSpeaking{
+    [self.speechSynthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
+}
 
-      if (myarg != nil) {
-          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-      } else {
-          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
-      }
-      [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-  }
+
+- (void) playSyntheticVoice:(NSString*) text usingLangage:(NSString*) language{
+    
+    AVSpeechSynthesisVoice *voice;
+    
+    if (language==nil){
+        voice = [AVSpeechSynthesisVoice voiceWithLanguage: [AVSpeechSynthesisVoice currentLanguageCode]];
+        
+    } else{
+        voice = [AVSpeechSynthesisVoice voiceWithLanguage: language];
+        
+    }
+    
+    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:text];
+    utterance.voice = voice;
+    utterance.rate *= 0.12;
+    [self.speechSynthesizer speakUtterance:utterance];
+}
+
+
+
+
+# pragma mark CDVPlugin
+
+
+- (void) startup:(CDVInvokedUrlCommand*) command {
+    
+    
+    [self.commandDelegate runInBackground:^{
+          self.speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+        [self playSyntheticVoice:@"Synthetizer is ready" usingLangage:@"en-GB"];
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        // The sendPluginResult method is thread-safe.
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+    
+}
+
+
+- (void) shutdown:(CDVInvokedUrlCommand*) command {
+    
+    
+    [self.commandDelegate runInBackground:^{
+        self.speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+        [self playSyntheticVoice:@"Synthetizer is ready" usingLangage:@"en-GB"];
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        // The sendPluginResult method is thread-safe.
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+    
+}
+
+
+
+- (void) speak:(CDVInvokedUrlCommand*) command {
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        [self playSyntheticVoice:@"Speak is running" usingLangage:@"en-GB"];
+        // The sendPluginResult method is thread-safe.
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+
+
+
+- (void) cancel:(CDVInvokedUrlCommand*) command {
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+       
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+
+- (void) pause:(CDVInvokedUrlCommand*) command {
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+
+- (void) resume:(CDVInvokedUrlCommand*) command {
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+
+- (void) stop:(CDVInvokedUrlCommand*) command {
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void) isLanguageAvailable:(CDVInvokedUrlCommand*) command {
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+
+@end
